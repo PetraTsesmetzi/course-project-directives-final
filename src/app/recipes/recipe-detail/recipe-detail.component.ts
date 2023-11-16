0c3c8df77bed4bb1d6e3e23eb5773a2caacb1c25
tsesme01 <petra.tsesmetzi.ext@ptb.de>
2023-11-16 11:20:45 +0100

    routing complete

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, } from '@angular/core';
-import { ActivatedRoute,Params} from '@angular/router';
+import { ActivatedRoute,Params, Router} from '@angular/router';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../receipe.service';
 
@@ -13,7 +13,8 @@ export class RecipeDetailComponent implements OnInit {
  id:number;
 
   constructor(private recipeService: RecipeService,
-    private route:ActivatedRoute) {}
+    private route:ActivatedRoute,
+    private router:Router) {}
 
   ngOnInit() {
     // const id=this.route.snapshot.params['id'];
@@ -27,5 +28,9 @@ export class RecipeDetailComponent implements OnInit {
   }
   onAddToShoppingList() {
     this.recipeService.addIngredientsTroShoppingList(this.recipe.ingredients);
+  }
+  onEditRecipe(){
+      // this.router.navigate(['edit'], {relativeTo:this.route});
+      this.router.navigate(["../",+this.id,'edit'], {relativeTo:this.route});
   }
 }
